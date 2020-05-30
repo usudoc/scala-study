0fa8b8bf50499ae54a7f2e18d3b5acd65dbe50f6
usudoc <yslan380@gmail.com>
2020-05-30 23:37:14 +0900

    edit chap03

--- src/main/scala/chap03/Main.scala
+++ src/main/scala/chap03/Main.scala
@@ -3,6 +3,6 @@ package chap03
 object Main {
 
   def main(args: Array[String]): Unit = {
-
+    Person.printWeight()
   }
 }
--- src/main/scala/chap03/Person.scala
+++ src/main/scala/chap03/Person.scala
@@ -1,5 +1,13 @@
 package chap03
 
-class Person {
+class Person(name: String, age: Int, private val weight: Int)
+// 以下のようにクラスのフィールドをprivate[this]にするとアクセスできず、エラーとなる
+// class Person(name: String, age: Int, private[this] val weight: Int)
 
+object Person {
+  def printWeight(): Unit = {
+    val taro = new Person("Taro", 20, 70)
+    println(taro.weight)
+  }
 }
+
